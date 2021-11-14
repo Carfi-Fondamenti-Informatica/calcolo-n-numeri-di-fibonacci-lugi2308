61f0e1715dea9679892c0835d873fec76b357901
lugi2308 <91835787+lugi2308@users.noreply.github.com>
2021-11-14 21:00:28 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n=0;
+    cin >>n;
+
+    if (n<2){
+        cout << "errore" << endl;
+
+    }else {
+        int a=0;
+        int b=1;
+        int c=0;
+            cout << b << endl;
+        for (int i=0;i<=(n-2);i++){
+            c=a+b;
+            a=b;
+            b=c;
+            cout << c << endl;
+
+        }
+    }
+    return 0;
 }
